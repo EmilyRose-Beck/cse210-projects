c575610ca1e733fd9b5c935217442efebf88f863
EmilyRose-Beck <bec20032@byui.edu>
2025-09-20 15:55:50 -0600

    Turned into Names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
+/*What is your first name? Scott
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.*/
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Enter first name: ");
+        string first_name = Console.ReadLine();
+        string upper_first = first_name.ToUpper();
+
+        Console.Write("Enter last name: ");
+        string last_name = Console.ReadLine();
+        string upper_last = last_name.ToUpper();
+        
+        Console.WriteLine("Your name is "+ upper_last + ", " + upper_first + " " + upper_last + ".");
     }
 }
