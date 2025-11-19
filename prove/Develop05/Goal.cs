ad4d70a38de52d52cca9e4d7b1be0242223d7800
EmilyRose-Beck <bec20032@byui.edu>
2025-11-19 08:18:29 -0700

    Added a Pet class, add like two lines of code to Goal.cs

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,2 +1,27 @@
 //parent class for the other goal classes
-Console.WriteLine("Testing");
+class Goal
+{
+    protected int _pointWorth;
+
+    //_option : string<List>
+
+    //points : int
+    public Goal(int pointWorth)
+    {
+        _pointWorth = pointWorth;
+
+    }
+    //checkbox : string  I want to use the rewrite for this one, come back later
+
+
+    //description : string
+
+    //isComplete: bool
+
+    //DisplayPoints()
+
+    //Save()
+
+    //IsComplete()
+}
+
