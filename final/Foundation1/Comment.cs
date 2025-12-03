ea9cca04b2cc6613a28539ac122b2d11378e7961
EmilyRose-Beck <bec20032@byui.edu>
2025-12-03 08:03:36 -0700

    outline

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,17 @@
+/*
+A comment should be defined by the Comment class which
+   has the responsibility for tracking both the name of the person
+    who made the comment and the text of the comment.
+*/
+
+using System
+
+class Comment
+{
+    // - _commentText: string()
+    // - _commentName: string()
+
+    // + Comment ()
+    // + GetCommenterName()
+    // + DisplayComments()
+}
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,21 @@
+/*
+Your program should have a class for a Video that has the 
+responsibility to track the title, author, and length (in seconds)
+ of the video. Each video also has responsibility to store a list
+  of comments, and should have a method to return the number of 
+  comments.
+*/
+using System;
+
+class Video
+{
+    // -_title: string()
+    // - _author: string()
+    // - _secondsTimed: float()
+    // - _comments: List<Comment>
+
+    //+ Video (title, author, seconds)
+    //+ GetNumberOfComments()
+    //+ AddComment()
+    //+ DisplayVideo()
+}
