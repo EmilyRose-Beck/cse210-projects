4a7cbb531d4e4e17a3501a9e0b63a9f437bf2148
EmilyRose-Beck <bec20032@byui.edu>
2025-12-06 15:07:18 -0700

    Product.cs is hopefully done and starting on Order.cs

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -3,10 +3,19 @@ using System;
 class Order
 {
     // - _customer: Customer
+    private Customer _customer;
     // - _products: List<Product>
+    private List<Product> _product;
     // - _shippingCost: float
+    private float _shippingCost;
 
     //Order(Customer customer, List<Product> products)
+    public Order(Customer customer, List<Product> product)
+    {
+        _customer = customer;
+        _product = product;
+    }
+
 
     //+ TotalPrice()
     // + shippingLabel()
--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -2,20 +2,44 @@ using System;
 
 class Product
 {
-    //- _howManySelected: int
-    private int _howManySelected;
     //- _name: string
     private string _name;
     // - _productID: int
     private int _productID;
     // - _pricePerItem: float
     private float _pricePerItem;
+    //- _howManySelected: int
+    private int _howManySelected;
 
     //+ Product(name, ID, price, quanity)
+    public Product(string name, int productID, float pricePerItem, int howManySelected)
+    {
+        _name = name;
+        _productID = productID;
+        _pricePerItem = pricePerItem;
+        _howManySelected = howManySelected;
+    }
     //+ GetPrice()
-    //return _howManySelected * _pricePerItem;
+    public float GetPrice()
+    {
+        //return _howManySelected * _pricePerItem;
+        return _howManySelected * _pricePerItem;
+    }
+
     //perhaps add?
     //GetName()
+    public string GetName()
+    {
+        return _name;
+    }
     //GetQuantity()
+    public int GetQuantity()
+    {
+        return _howManySelected;
+    }
     //GetProductID()
+    public int GetProductID()
+    {
+        return _productID;
+    }
 }
