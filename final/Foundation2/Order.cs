b66552e1e9336c47976f9aa1d23828753dd4c8e1
EmilyRose-Beck <bec20032@byui.edu>
2025-12-06 15:18:34 -0700

    Order is done :)

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -5,20 +5,58 @@ class Order
     // - _customer: Customer
     private Customer _customer;
     // - _products: List<Product>
-    private List<Product> _product;
+    private List<Product> _products;
     // - _shippingCost: float
     private float _shippingCost;
 
     //Order(Customer customer, List<Product> products)
-    public Order(Customer customer, List<Product> product)
+    public Order(Customer customer, List<Product> products)
     {
         _customer = customer;
-        _product = product;
+        _products = products;
+
+        //+ shippingCost()
+        if (_customer.GetAddress().IsUsa())
+        {
+            _shippingCost = 5f;
+        }
+        else
+        {
+            _shippingCost = 35f;
+        }
     }
 
 
     //+ TotalPrice()
+    public float TotalPrice()
+    {
+        float total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetPrice();
+        }
+
+        total += _shippingCost;
+        return total;
+    }
+
     // + shippingLabel()
+    public string shippingCost()
+    {
+        return $"{_customer.GetCustomerName()}\n{_customer.GetShippingAddress()}";
+    }
     // + PackingLabel()
+    public string PackingLabel()
+    {
+        string label = "";
+
+        foreach (Product product in _products)
+        {
+            label += $"Product: {product.GetName()}, ID: {product.GetProductID()}\n";
+        }
+        return label;
+    }
+
 
 }
