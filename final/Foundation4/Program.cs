f9903e38a7fce459c7d0f821197f8f0b18190cdf
EmilyRose-Beck <bec20032@byui.edu>
2025-12-11 16:15:04 -0700

    Program is done and everything currently works

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("11 Dec 2025", 30f, 3f);
+
+        Cycling cycling = new Cycling("10 Dec 2025", 45f, 12f);
+        Swimming swimming = new Swimming("09 Dec 2025", 40f, 50);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        for (int i = 0; i < activities.Count; i++)
+        {
+            Console.WriteLine(activities[i].GetSummary());
+            Console.WriteLine();
+        }
     }
 }
